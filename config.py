6a202bd52c5bb6db05a4a019d4bb8a67a04eb460
Tom Brennan <40214498+thomasbrennan@users.noreply.github.com>
2018-09-26 21:28:25 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "hGoNcOxbFA4H8T2vz3dxDKlWW"
+consumer_secret = "mPEt64dm7dvvqz1P2HSKZOfGBSeaoyA7sTnIoHb8jFHa8rqe80"
+access_token = "901799263910453249-RCFDbqHdZlClCS36UCqxkRht823MHXh"
+access_token_secret = "eHHShnNSLLVZyApv8OQOzLRWspSBuCziPsS08fxuiJu1i"
